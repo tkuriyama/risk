2e83f73cfd965dfc2098bbac4fe581863696d4d2
tkuriyama <taro.kuriyama@gmail.com>
2020-09-17 17:16:38 -0400

    Adding TCR scripts

--- elm/tests/Example.elm
+++ elm/tests/Example.elm
@@ -7,4 +7,4 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    todo "Test basic Raitional creation"
