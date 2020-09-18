6f3087880e0f134063d8d92d4f9ccfd5427d2f78
tkuriyama <taro.kuriyama@gmail.com>
2020-09-18 08:50:13 -0400

    fiixing gcd syntax

--- elm/src/Rational.elm
+++ elm/src/Rational.elm
@@ -9,23 +9,23 @@ type Rational = Rational { num : Numerator
                          , denom : Denominator }
               | NotRational String
 
+zero = BI.fromInt 0
+                
 fromInt : Int -> Int -> Rational
 fromInt n d = Rational { num = BI.fromInt n, denom = BI.fromInt d }          
 
 fromBigInt : BI.BigInt -> BI.BigInt -> Rational
 fromBigInt n d = Rational { num = n, denom = d }
                  
-gcd :: BigInt -> BigInt -> BigInt
-gcd a b = if a == b then a
-          else let m = BI.modBy a b
-               in case m of
-                      (Just n) -> gcd n
-                      Nothing -> 0
-                 
+gcd : BI.BigInt -> BI.BigInt -> BI.BigInt
+gcd a b = if b == zero then a
+          else case (BI.modBy a b) of
+                   (Just m) -> gcd b m
+                   Nothing -> zero
+
 -- function gcd(a, b)
---     while a ≠ b 
---         if a > b
---             a := a − b
---         else
---             b := b − a
---     return a      
+--    if b = 0
+--        return a
+--    else
+--        return gcd(b, a mod b)                 
+
