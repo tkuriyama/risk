3b07366df482b9eadde14a2441c437e06e7650d4
tkuriyama <taro.kuriyama@gmail.com>
2020-09-22 16:24:27 -0400

    basic types

--- elm/src/Risk.elm
+++ elm/src/Risk.elm
@@ -1,6 +1,13 @@
 module Risk exposing (..)
 
 
+type alias DieValue = Int
+type alias Army = Int
+type alias Losses = (Army, Army)    
+type alias Battlefield = (Army, Army)
+
+
+
 risk : String
 risk = ""
 
